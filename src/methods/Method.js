7479848b2259dc25b83d5d409605a021da03d160
AJAY H. SAMPAT <sampata@us.ibm.com>
2021-12-12 10:19:02 -0500

    added code for recent commits (at least 25)

--- src/methods/Method.js
+++ src/methods/Method.js
@@ -4,15 +4,15 @@ const fetch = require("cross-fetch");
 
 async function fetchData(uname,repo) {
 console.log('https://api.github.com/repos/'+uname+'/'+repo+'/commits');
-const headers = {
+/*const headers = {
     //'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*'
   //  "Accept": "application/vnd.github.v3+json",
-  };
+  };*/
     const response = await fetch('https://api.github.com/repos/'+uname+'/'+repo+'/commits');
    const data = await response.json();
   
-
-return data;
+//recent commits (at least 25)
+return data.splice(0, 25);
 }
 module.exports=fetchData;
