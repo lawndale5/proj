0abf66a84507c0fbb89cd24f526dab5e0e5749d8
AJAY H. SAMPAT <sampata@us.ibm.com>
2021-12-13 19:05:46 -0500

    fixed bug if user enters invalid data

--- src/methods/Method.js
+++ src/methods/Method.js
@@ -13,8 +13,12 @@ console.log('https://api.github.com/repos/'+uname+'/'+repo+'/commits');
    const data = await response.json();
   
 //recent commits (at least 25)
-return data.splice(0, 25).map(_data => {
-  return { sha: _data.sha, message: _data.commit.message ,name: _data.commit.author.name};})
+  if (data.length){
+    return data.splice(0, 25).map(_data => {
+      return { sha: _data.sha, message: _data.commit.message ,name: _data.commit.author.name};})
+    }
+  else
+    return data;
 }
 module.exports=fetchData;
 
